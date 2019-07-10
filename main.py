35de294c4a93dd16dbf1f288f8e772e1d712ff33
unknown <martinye022@gmail.com>
2019-07-10 19:38:03 -0400

    data formatting

--- main.py
+++ main.py
@@ -9,8 +9,13 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from scipy.io import arff
 import pandas as pd
+import os
 
-
-
-
-#split data into test and train
+#access to data
+os.chdir(r"C:\Users\User\Desktop")
+storeData = pd.read_csv('Sales_Transactions_Dataset_Weekly.csv')
+#format data
+df = pd.DataFrame(storeData)
+#split data into test and train - p1 (first 700 no prediction last 100 50 days train 1 day test)
+#x - train 50 days
+#y - test 1 day

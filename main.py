11a2f876515489b0f94a5a346866daeb118a87e4
unknown <martinye022@gmail.com>
2019-07-11 15:59:14 -0400

    finished basic product - tweaking next

--- main.py
+++ main.py
@@ -4,44 +4,41 @@ Aiming for 80% accuracy and plus
 """
 
 #import necessary modules
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
 from scipy.io import arff
 import pandas as pd
 import os
-
+"""
+Organizing and formatting data
+"""
 #access to data
 os.chdir(r"C:\Users\User\Desktop")
 storeData = pd.read_csv('Sales_Transactions_Dataset_Weekly.csv')
 
 #format data
 df = pd.DataFrame(storeData)
-list(df)
-
-#x_train is the training from column 0 to 50 - THIS NEEDS TO BE A LIST  
-#xVar = df.loc[:,'Normalized 0':'Normalized 50']
-
-#formatting data
-xVar = df.drop(df.columns[0:55],axis = 1)
-xVar = xVar.drop('Normalized 51',axis = 1)
+#access only the data that is to be used
+xVar = df.drop('Normalized 51',axis = 1)
+for i in range(52):
+    xVar = xVar.drop(('W'+str(i)),axis = 1)
+xVar = xVar.drop('MIN',axis = 1)
+xVar = xVar.drop('MAX',axis = 1)
+xVar = xVar.drop('Product_Code',axis = 1)
 yVar = df['Normalized 51']
 
 #splits data into 80% training and 20% testing
 X_train, X_test, y_train, y_test = train_test_split(xVar, yVar, test_size=0.2,shuffle = False)
-"""
-clf = RandomForestClassifier(n_jobs=2, random_state=0)
-#everything up to here works
-clf.fit(X_train, y_train)
 
-RandomForestClassifier(bootstrap=True, class_weight=None, criterion='gini',
-            max_depth=None, max_features='auto', max_leaf_nodes=None,
-            min_impurity_split=1e-07, min_samples_leaf=1,
-            min_samples_split=2, min_weight_fraction_leaf=0.0,
-            n_estimators=10, n_jobs=2, oob_score=False, random_state=0,
-            verbose=0, warm_start=False)
-
-predictions = clf.predict(X_test)
-#check accuracy
-pd.crosstab(y_test, predictions, rownames=['Actual Result'], colnames=['Predicted Result'])
 """
+Random Forest
+"""
+rnd_clf = RandomForestRegressor(n_estimators=500, max_leaf_nodes=16, n_jobs=-1)
+rnd_clf.fit(X_train, y_train)
+
+y_pred_rf = rnd_clf.predict(X_test)
+print(y_pred_rf)
+print(yVar)

62e1da53e8b01ddd8c3e5cc50a7a6efd5cefce65
unknown <martinye022@gmail.com>
2019-07-10 20:15:24 -0400

    x and y trains added

--- main.py
+++ main.py
@@ -16,6 +16,23 @@ os.chdir(r"C:\Users\User\Desktop")
 storeData = pd.read_csv('Sales_Transactions_Dataset_Weekly.csv')
 #format data
 df = pd.DataFrame(storeData)
+#x_train is the training - wip
+x_train = df["W0"]
+#y_train is the test
+y_train = df['Normalized 51']   
+print(df)
+print(x_train)
+
+
+"""
+xVar = df.loc['']
+
+:)
+
+yVar = df.iloc[:,20]
+df2 = df[xVar]
+"""
 #split data into test and train - p1 (first 700 no prediction last 100 50 days train 1 day test)
 #x - train 50 days
 #y - test 1 day
+

cbf20ae3301f4ed297c0cd52c243bf154855dfcf
unknown <martinye022@gmail.com>
2019-07-12 09:43:30 -0400

    started graphing, still need to format better

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ Aiming for 80% accuracy and plus
 """
 
 #import necessary modules
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 from scipy.io import arff
 import pandas as pd
 import os
@@ -21,6 +22,7 @@ storeData = pd.read_csv('Sales_Transactions_Dataset_Weekly.csv')
 
 #format data
 df = pd.DataFrame(storeData)
+
 #access only the data that is to be used
 xVar = df.drop('Normalized 51',axis = 1)
 for i in range(52):
@@ -30,15 +32,41 @@ xVar = xVar.drop('MAX',axis = 1)
 xVar = xVar.drop('Product_Code',axis = 1)
 yVar = df['Normalized 51']
 
+#used to check if
+
 #splits data into 80% training and 20% testing
-X_train, X_test, y_train, y_test = train_test_split(xVar, yVar, test_size=0.2,shuffle = False)
+X_train, X_test, y_train, y_test = train_test_split(xVar, yVar, test_size=0.2)
 
 """
 Random Forest
 """
-rnd_clf = RandomForestRegressor(n_estimators=500, max_leaf_nodes=16, n_jobs=-1)
+rnd_clf = RandomForestRegressor(n_estimators=1000, criterion="mse", n_jobs=-1)
+
 rnd_clf.fit(X_train, y_train)
 
-y_pred_rf = rnd_clf.predict(X_test)
-print(y_pred_rf)
-print(yVar)
+#make predictions
+y_pred = rnd_clf.predict(X_train)
+# print(mean_squared_error(y_test,y_pred))
+"""
+Graphing
+"""
+#Create rectangle   
+fig = plt.figure()
+#data to plot
+actual = [i for i in range(51)]
+print(list(df.iloc[:,106]))
+predictions = [i+1 for i in range(51)]
+#x axis is number of days which is 51 days from day 0 to 50
+x_axis = [i for i in range(51)]
+#label along y axis
+plt.ylabel("Units sold")
+#label along x axis
+plt.xlabel("Days")
+#title
+plt.title("Units actually sold and Units predicted")
+#plot data
+plt.plot(x_axis,actual)
+plt.plot(x_axis,predictions)
+plt.show()
+
+

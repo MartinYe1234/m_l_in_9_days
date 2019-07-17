962d47941466c92b96131b1432f5210576da2068
unknown <martinye022@gmail.com>
2019-07-17 18:39:58 -0400

    comments

--- ARIMA.py
+++ ARIMA.py
@@ -1,6 +1,7 @@
 """
 Using ARIMA to forecast Superstore sales
 """
+
 #Import statements
 import itertools
 import numpy as np
@@ -9,51 +10,57 @@ import pandas as pd
 import statsmodels.api as sm
 import matplotlib as mpl
 from pylab import rcParams
+
 #style of graph - purely asthetic
 plt.style.use('ggplot')
 #formatting of graphs - size of labels
 mpl.rcParams['axes.labelsize'] = 14
 mpl.rcParams['xtick.labelsize'] = 12
 mpl.rcParams['ytick.labelsize'] = 12
-#store df 
+#we are working with excel files
 df = pd.read_excel("Superstore.xls")
-print(df.head())
+
+"""
+Formatting data
+"""
+
 furniture = df.loc[df['Category'] == 'Furniture']
-print(furniture.head())
 furniture['Order Date'].min(), furniture['Order Date'].max()
+#get rid of every column except order date and sales
 cols = ['Row ID', 'Order ID', 'Ship Date', 'Ship Mode', 'Customer ID', 'Customer Name', 'Segment', 'Country', 'City', 'State', 'Postal Code', 'Region', 'Product ID', 'Category', 'Sub-Category', 'Product Name', 'Quantity', 'Discount', 'Profit']
 furniture.drop(cols, axis=1, inplace=True)
+#sort sales based on the date, from least to most recent
 furniture = furniture.sort_values('Order Date')
 furniture.isnull().sum()
 furniture = furniture.groupby('Order Date')['Sales'].sum().reset_index()
 furniture = furniture.set_index('Order Date')
 furniture.index
-print(furniture)
 y = furniture['Sales'].resample('MS').mean()
+
 """
 Use ARIMA
 """
-mod = sm.tsa.statespace.SARIMAX(y,
-                                order=(1, 1, 1),
-                                seasonal_order=(1, 1, 0, 12),
-                                enforce_stationarity=False,
-                                enforce_invertibility=False)
+
+mod = sm.tsa.statespace.SARIMAX(y, order=(1, 1, 1), seasonal_order=(1, 1, 0, 12), enforce_stationarity=False, enforce_invertibility=False)
 results = mod.fit()
 results.plot_diagnostics(figsize=(16, 8))
 plt.show()
+
+#generate predictions
 pred = results.get_prediction(start=pd.to_datetime('2017-01-01'), dynamic=False)
 pred_ci = pred.conf_int()
 ax = y['2014':].plot(label='observed')
 pred.predicted_mean.plot(ax=ax, label='One-step ahead Forecast', alpha=.7, figsize=(14, 7))
-ax.fill_between(pred_ci.index,
-                pred_ci.iloc[:, 0],
-                pred_ci.iloc[:, 1], color='k', alpha=.2)
+ax.fill_between(pred_ci.index, pred_ci.iloc[:, 0], pred_ci.iloc[:, 1], color='k', alpha=.2)
+
+#adding labels for the x and y axis
 ax.set_xlabel('Date')
 ax.set_ylabel('Grocery Sales')
 plt.legend()
 plt.show()
 y_forecasted = pred.predicted_mean
 y_truth = y['2017-01-01':]
+#means squared error
 mse = ((y_forecasted - y_truth) ** 2).mean()
 print('The Mean Squared Error of our forecasts is {}'.format(round(mse, 2)))
 print('The Root Mean Squared Error of our forecasts is {}'.format(round(np.sqrt(mse), 2)))
